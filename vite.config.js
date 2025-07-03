7cab5102ca6c1d1d79a90c62bc966cc5a59d70d0
Arbaj-coder <2024kucp1060@iiitkota.ac.in>
2025-07-03 21:26:23 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,22 +1,22 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import { resolve } from 'path';
-import { copyFileSync } from 'fs';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import { copyFileSync } from 'fs'
 
 export default defineConfig({
   plugins: [react()],
   build: {
     rollupOptions: {
-      // hook to copy _redirects manually
       output: {
-        // after build is complete
-        plugins: [{
-          name: 'copy-redirects',
-          generateBundle() {
-            copyFileSync('public/_redirects', 'dist/_redirects');
-          }
-        }]
-      }
-    }
-  }
-});
+        plugins: [
+          {
+            name: 'copy-redirects',
+            generateBundle() {
+              copyFileSync('public/_redirects', 'dist/_redirects')
+            },
+          },
+        ],
+      },
+    },
+  },
+})
+
